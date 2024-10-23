d128487f993ddc264e37382564dce7389b3bc804
RoDriguezBSIT3-2 <132137046+RoDriguezBSIT3-2@users.noreply.github.com>
2024-10-23 21:21:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Configure PostgreSQL Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:984832rodriguez@localhost:5432/Inventory'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgresql_17_user:0L5e975YcqS0bht9judwD2fpT9webEGZ@localhost:5432/postgresql_17'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Example in-memory storage for demonstration
